1a57144df9d006020524ab164cc91718aabad316
Alan <csuen2@my.centennialcollege.ca>
2023-03-07 18:21:05 -0400

    Init commit

---
+++ app/src/main/java/com/cfs/bot/mapd721_t1_chunfungsuen/MainActivity.java
@@ -0,0 +1,14 @@
+package com.cfs.bot.mapd721_t1_chunfungsuen;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
