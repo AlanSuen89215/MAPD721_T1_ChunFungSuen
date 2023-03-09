4c552b0ae48c36b1b784e6e06dc1eebdb7990a13
Alan <csuen2@my.centennialcollege.ca>
2023-03-08 21:48:40 -0400

    Fix notification do not display

--- app/src/main/java/com/cfs/bot/mapd721_t1_chunfungsuen/MainActivity.java
+++ app/src/main/java/com/cfs/bot/mapd721_t1_chunfungsuen/MainActivity.java
@@ -6,9 +6,14 @@ import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.ListView;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import com.cfs.bot.mapd721_t1_chunfungsuen.adapter.ChatMessageListViewAdapter;
+import com.cfs.bot.mapd721_t1_chunfungsuen.service.ChatBotService;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -17,11 +22,14 @@ public class MainActivity extends AppCompatActivity {
     private ChatBotBroadcastReceiver chatBotBroadcastReceiver;
     private ListView chatMessageListView;
     private List<String> chatMessageList = new ArrayList<>();
+    private Context context;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        context = this;
 
         // DEBUG
         chatMessageList.add("Hello");
@@ -33,7 +41,31 @@ public class MainActivity extends AppCompatActivity {
                 chatMessageList
         ));
         chatBotBroadcastReceiver = new ChatBotBroadcastReceiver();
+
+        ((Button) findViewById(R.id.btn_generate_msg))
+                .setOnClickListener(btnGenerateMessageOnClickListener);
     }
+
+    private View.OnClickListener btnGenerateMessageOnClickListener = new View.OnClickListener() {
+
+        @Override
+        public void onClick(View view) {
+            String userName = ((TextView) findViewById(R.id.user))
+                    .getText().toString();
+
+            if (userName.trim().equals("")) {
+                Toast.makeText(context, "user name is empty", Toast.LENGTH_SHORT)
+                        .show();
+            }
+
+            Bundle data = new Bundle();
+            data.putInt(ChatBotService.CMD, ChatBotService.CMD_GENERATE_MESSAGE);
+            data.putString(ChatBotService.KEY_USER_NAME, userName);
+            Intent intent = new Intent(context, ChatBotService.class);
+            intent.putExtras(data);
+            startService(intent);
+        }
+    };
 
     public static class ChatBotBroadcastReceiver extends BroadcastReceiver {
         @Override
--- app/src/main/java/com/cfs/bot/mapd721_t1_chunfungsuen/notification/NotificationDecorator.java
+++ app/src/main/java/com/cfs/bot/mapd721_t1_chunfungsuen/notification/NotificationDecorator.java
@@ -1,6 +1,9 @@
 package com.cfs.bot.mapd721_t1_chunfungsuen.notification;
 
+import static android.app.PendingIntent.FLAG_IMMUTABLE;
+
 import android.app.Notification;
+import android.app.NotificationChannel;
 import android.app.NotificationManager;
 import android.app.PendingIntent;
 import android.content.Context;
@@ -15,24 +18,34 @@ import java.util.Date;
 
 public class NotificationDecorator {
     private static final String TAG = "NotificationDecorator";
+    private String channelId = "chat_bot_channel";
+    private String channelName = "Chat Bot Channel";
     private final Context context;
     private final NotificationManager notificationMgr;
+
+    private static int notificationId = 0;
 
     public NotificationDecorator(Context context, NotificationManager notificationManager) {
         this.context = context;
         this.notificationMgr = notificationManager;
+
+        NotificationChannel channel = new NotificationChannel(
+                channelId,
+                channelName,
+                NotificationManager.IMPORTANCE_HIGH);
+        notificationMgr.createNotificationChannel(channel);
     }
 
     public void displayExpandableNotification(String title, String contentText) {
-        contentText = contentText + "\n"
+        /*contentText = contentText + "\n"
                 + "Chat Bot\n"
-                + (new SimpleDateFormat("MM/dd/yyyy hh:mm:ss")).format(new Date());
+                + (new SimpleDateFormat("MM/dd/yyyy hh:mm:ss")).format(new Date());*/
 
         Intent intent = new Intent(context, MainActivity.class);
         intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP |
                 Intent.FLAG_ACTIVITY_SINGLE_TOP);
         PendingIntent contentIntent = PendingIntent.getActivity(context, 0,
-                intent, PendingIntent.FLAG_UPDATE_CURRENT);
+                intent, PendingIntent.FLAG_UPDATE_CURRENT | FLAG_IMMUTABLE);
 
         try {
             Notification notification = new Notification.Builder(context)
@@ -42,9 +55,11 @@ public class NotificationDecorator {
                     .setContentIntent(contentIntent)
                     .setAutoCancel(true)
                     .setLights(Color.BLUE, 1000, 1000)
+                    .setChannelId(channelId)
                     .build();
             notification.flags |= Notification.FLAG_AUTO_CANCEL;
-            notificationMgr.notify(0, notification);
+            notificationMgr.notify(notificationId, notification);
+            notificationId += 1;
         } catch (IllegalArgumentException e) {
             Log.e(TAG, e.getMessage());
         }
